b1923bed9343b77567b7c8f3e8f97c467e3bd611
plcoirier <plcoirier@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2009-09-14 16:51:33 +0000

    remove useless interface

--- v1.0/examples/EmployeeAdmin/src/com/mvp4g/example/client/UserServiceAsync.java
+++ v1.0/examples/EmployeeAdmin/src/com/mvp4g/example/client/UserServiceAsync.java
@@ -3,10 +3,9 @@ package com.mvp4g.example.client;
 import java.util.List;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.rpc.RemoteService;
 import com.mvp4g.example.client.bean.UserBean;
 
-public interface UserServiceAsync extends RemoteService {
+public interface UserServiceAsync {
 	
 	public void getUsers(AsyncCallback<List<UserBean>> callback);
 	public void deleteUser(UserBean user, AsyncCallback<Void> callback);
