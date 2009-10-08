7eb1bf4145e472d373082f758370b66911924a04
plcoirier <plcoirier@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2009-10-08 16:12:12 +0000

    commit Mvp4g Performances project

---
+++ trunk/v1.0/examples/Mvp4gPerformances/src/com/mvp4g/example/client/events/Event1.java
@@ -0,0 +1,34 @@
+package com.mvp4g.example.client.events;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+
+public class Event1 extends GwtEvent<Event1.Event1Handler> {
+	
+	public interface Event1Handler extends EventHandler{
+		void handle(Event1 event);
+	}
+	
+	public static final GwtEvent.Type<Event1.Event1Handler> TYPE = new GwtEvent.Type<Event1.Event1Handler>();
+	
+	private String event = null;
+	
+	public Event1(String event){
+		this.event = event;
+	}
+	
+	public String getEvent(){
+		return event;
+	}
+
+	@Override
+	protected void dispatch( Event1Handler handler ) {
+			handler.handle( this );	
+	}
+
+	@Override
+	public Type<Event1.Event1Handler> getAssociatedType() {
+		return TYPE;
+	}
+
+}
