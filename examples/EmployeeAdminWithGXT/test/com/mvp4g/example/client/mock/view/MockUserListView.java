f500d70488485bbb9f67ed4835873b44a525ea0d
plcoirier <plcoirier@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2009-10-17 20:56:16 +0000

    add GWTMockUtilities to test the User List presenter

--- examples/EmployeeAdminWithGXT/test/com/mvp4g/example/client/mock/view/MockUserListView.java
+++ examples/EmployeeAdminWithGXT/test/com/mvp4g/example/client/mock/view/MockUserListView.java
@@ -1,21 +1,28 @@
 package com.mvp4g.example.client.mock.view;
 
+import com.extjs.gxt.ui.client.data.BeanModel;
+import com.extjs.gxt.ui.client.store.ListStore;
 import com.google.gwt.user.client.ui.Widget;
 import com.mvp4g.example.client.mock.widget.MyMockLabel;
 import com.mvp4g.example.client.mock.widget.gxt.MyGXTMockButton;
+import com.mvp4g.example.client.mock.widget.gxt.MyGXTMockPagingToolBar;
 import com.mvp4g.example.client.mock.widget.gxt.MyGXTMockTable;
 import com.mvp4g.example.client.presenter.view_interface.UserListViewInterface;
 import com.mvp4g.example.client.presenter.view_interface.widget_interface.MyLabelInterface;
 import com.mvp4g.example.client.presenter.view_interface.widget_interface.MyWidgetInterface;
+import com.mvp4g.example.client.presenter.view_interface.widget_interface.gxt.MyGXTPagingToolBarInterface;
 
 public class MockUserListView implements UserListViewInterface, MyWidgetInterface {
-	
+
 	private MyLabelInterface confirmText = new MyMockLabel();
 	private MyGXTMockButton deleteButton = new MyGXTMockButton();
 	private MyGXTMockButton newButton = new MyGXTMockButton();
 	private MyGXTMockButton yesButton = new MyGXTMockButton();
 	private MyGXTMockButton noButton = new MyGXTMockButton();
 	private MyGXTMockTable table = new MyGXTMockTable();
+	private MyGXTMockPagingToolBar toolBar = new MyGXTMockPagingToolBar();
+	
+	private ListStore<BeanModel> store = null;	
 
 	public MyLabelInterface getConfirmText() {
 		return confirmText;
@@ -47,6 +54,18 @@ public class MockUserListView implements UserListViewInterface, MyWidgetInterfac
 
 	public Widget getMyWidget() {
 		return null;
+	}
+
+	public void buildWidget( ListStore<BeanModel> store ) {
+		this.store = store;
+	}
+
+	public MyGXTPagingToolBarInterface getToolBar() {
+		return toolBar;
+	}
+	
+	public ListStore<BeanModel> getStore(){
+		return store;
 	}
 
 }
