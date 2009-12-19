b2a70a4177e3e6f43a1cfed133a8e76b69bb4682
plcoirier <plcoirier@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2009-12-19 13:58:01 +0000

    Add module feature for XML

---
+++ sources/src/main/java/com/mvp4g/util/config/loader/xml/ChildModuleLoader.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.mvp4g.util.config.loader.xml;
+
+import org.apache.commons.configuration.XMLConfiguration;
+
+import com.mvp4g.util.config.element.ChildModuleElement;
+
+/**
+ * A class responsible for loading all Services defined in the configuration
+ * file.
+ * 
+ * @author javier
+ * 
+ */
+public class ChildModuleLoader extends Mvp4gElementLoader<ChildModuleElement> {
+
+	static final String[] REQUIRED_ATTRIBUTES = { "name", "class",
+			"eventToLoadView" };
+	static final String[] PARENT_ATTRIBUTES = { "package" };
+	static final String[] OPTIONAL_ATTRIBUTES = { "async" };
+
+	@SuppressWarnings("unchecked")
+	public ChildModuleLoader(XMLConfiguration xmlConfig) {
+		super(xmlConfig.configurationsAt("childModules.childModule"));
+	}
+
+	@Override
+	protected String getElementLabel() {
+		return "ChildModule";
+	}
+
+	@Override
+	protected String[] getRequiredAttributeNames() {
+		return REQUIRED_ATTRIBUTES;
+	}
+
+	@Override
+	protected ChildModuleElement newElement() {
+		return new ChildModuleElement();
+	}
+
+	@Override
+	protected String[] getParentAttributeNames() {
+		return PARENT_ATTRIBUTES;
+	}
+
+	@Override
+	String[] getOptionalAttributeNames() {
+		return OPTIONAL_ATTRIBUTES;
+	}
+
+}
