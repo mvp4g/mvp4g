1f03c11f1f366e603acdf21edbd0396a658a7e7d
plcoirier <plcoirier@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2009-12-19 14:02:14 +0000

    Add module feature for XML

---
+++ trunk/examples/Mvp4gModulesXml/src/com/mvp4g/example/client/company/CompanyModule.java
@@ -0,0 +1,9 @@
+package com.mvp4g.example.client.company;
+
+import com.mvp4g.client.Mvp4gModule;
+import com.mvp4g.client.annotation.XmlFilePath;
+
+@XmlFilePath("xmlConfig/company-mvp4g.xml")
+public interface CompanyModule extends Mvp4gModule {
+
+}
---
+++ trunk/examples/Mvp4gModulesXml/src/com/mvp4g/example/client/main/MainPresenter.java
@@ -0,0 +1,67 @@
+package com.mvp4g.example.client.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.ui.Widget;
+import com.mvp4g.client.Mvp4gModule;
+import com.mvp4g.client.annotation.Presenter;
+import com.mvp4g.client.presenter.XmlPresenter;
+
+@Presenter(view = MainView.class)
+public class MainPresenter extends
+		XmlPresenter<MainPresenter.MainViewInterface> {
+
+	public Map<Class<? extends Mvp4gModule>, Mvp4gModule> modules = new HashMap<Class<? extends Mvp4gModule>, Mvp4gModule>();
+
+	public interface MainViewInterface {
+
+		public HasClickHandlers getCompanyMenu();
+
+		public HasClickHandlers getProductMenu();
+
+		public void setBody(Widget newBody);
+		
+		public void displayErrorMessage(String error);
+		public void setWaitVisible(boolean visible);
+	
+	}
+
+	public void bind() {
+		view.getCompanyMenu().addClickHandler(new ClickHandler() {
+
+			public void onClick(ClickEvent event) {
+				eventBus.dispatch("goToCompany");
+			}
+		});
+		view.getProductMenu().addClickHandler(new ClickHandler() {
+
+			public void onClick(ClickEvent event) {
+				eventBus.dispatch("goToProduct");
+			}
+		});	
+		
+	}
+
+	public void onChangeBody(Widget w) {
+		view.setBody(w);
+	}
+	
+	public void onErrorOnLoad(Throwable reason){
+		view.displayErrorMessage(reason.getMessage());
+	}
+	
+	public void onBeforeLoad(){
+		view.setWaitVisible(true);
+	}
+	
+	public void onAfterLoad(){
+		view.setWaitVisible(false);
+	}
+	
+	
+
+}
