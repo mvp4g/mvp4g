d54b154a16e6608a9676e8acf02344b41a8a95af
plcoirier <plcoirier@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2010-01-30 16:45:20 +0000

    integrate last version of Mvp4g

--- trunk/examples/Mvp4gModulesXml/src/com/mvp4g/example/client/company/CompanyModule.java
+++ trunk/examples/Mvp4gModulesXml/src/com/mvp4g/example/client/company/CompanyModule.java
@@ -2,11 +2,10 @@ package com.mvp4g.example.client.company;
 
 import com.mvp4g.client.Mvp4gModule;
 import com.mvp4g.client.annotation.XmlFilePath;
-import com.mvp4g.client.event.EventBusWithLookup;
 
 @XmlFilePath("xmlConfig/company-mvp4g.xml")
 public interface CompanyModule extends Mvp4gModule {
 	
-	public void setParentEventBus(EventBusWithLookup eventBus);
+	public void setParentModule(Mvp4gModule eventBus);
 
 }
--- trunk/examples/Mvp4gModulesXml/src/com/mvp4g/example/client/product/ProductModule.java
+++ trunk/examples/Mvp4gModulesXml/src/com/mvp4g/example/client/product/ProductModule.java
@@ -2,11 +2,10 @@ package com.mvp4g.example.client.product;
 
 import com.mvp4g.client.Mvp4gModule;
 import com.mvp4g.client.annotation.XmlFilePath;
-import com.mvp4g.client.event.EventBusWithLookup;
 
 @XmlFilePath("xmlConfig/product-mvp4g.xml")
 public interface ProductModule extends Mvp4gModule {
 	
-	public void setParentEventBus(EventBusWithLookup eventBus);
+	public void setParentModule(Mvp4gModule eventBus);
 	
 }
