862b0dacbc046e6fab158dff3703bcfcf76e34f1
plcoirier <plcoirier@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2010-09-11 03:13:54 +0000

    update examples with the last version

--- trunk/examples/Mvp4gShowCase/src/com/mvp4g/example/client/MiddlePanel.java
+++ trunk/examples/Mvp4gShowCase/src/com/mvp4g/example/client/MiddlePanel.java
@@ -10,6 +10,7 @@ public class MiddlePanel extends Composite {
 
 	private HTML description = new HTML();
 	private Frame demo = new Frame();
+	private Frame eventbus = new Frame();
 	private Frame configuration = new Frame();
 	private TabPanel tabPanel = new TabPanel();
 
@@ -21,7 +22,7 @@ public class MiddlePanel extends Composite {
 		mainPanel.add( description );
 
 		tabPanel.add( demo, "Demo" );
-		tabPanel.add( configuration, "Mvp4g Configuration file" );
+		tabPanel.add( eventbus, "Event Bus Interface" );
 
 		mainPanel.add( tabPanel );
 
@@ -29,9 +30,28 @@ public class MiddlePanel extends Composite {
 
 	public void set( MenuItem item ) {
 		description.setHTML( "<h2>" + item.getProjectName() + "</h2>" + item.getDescription() );
-		demo.setUrl( "http://" + item.getAppId() + ".appspot.com/" );
-		configuration.setUrl( "http://mvp4g.googlecode.com/svn/tags/mvp4g-1.0.0/examples/" + item.getProjectName() + "/src/mvp4g-conf.xml" );
+		String appId = item.getAppId();
+		if ( appId.startsWith( "http" ) ) {
+			demo.setUrl( appId );
+		} else {
+			demo.setUrl( "http://" + item.getAppId() + ".appspot.com/" );
+		}
+		eventbus.setUrl( "http://code.google.com/p/mvp4g/source/browse/tags/mvp4g-1.1.0/examples/" + item.getProjectName()
+				+ "/src/com/mvp4g/example/client/" + item.getEventBusName() + ".java" );
 		tabPanel.selectTab( 0 );
+
+		int nbTab = tabPanel.getTabBar().getTabCount();
+		if ( !item.isWithXml() ) {
+			if ( nbTab == 3 ) {
+				tabPanel.remove( configuration );
+			}
+		} else {
+			configuration.setUrl( "http://mvp4g.googlecode.com/svn/tags/mvp4g-1.1.0/examples/" + item.getProjectName() + "Xml/src/mvp4g-conf.xml" );
+			if ( nbTab < 3 ) {
+				tabPanel.add( configuration, "Mvp4g XML Configuration" );
+			}
+		}
+
 	}
 
 }
--- trunk/examples/Mvp4gShowCase/src/com/mvp4g/example/client/Mvp4gShowCase.java
+++ trunk/examples/Mvp4gShowCase/src/com/mvp4g/example/client/Mvp4gShowCase.java
@@ -44,20 +44,34 @@ public class Mvp4gShowCase implements EntryPoint {
 
 	private void initMenu( Tree menu ) {
 		//TestMvp4g
-		String d1 = "A basic example to easily understand how the framework works.";
-		menu.addItem( new MenuItem( d1, "testmvp4g", "TestMvp4g" ) );
-		
+		String d1 = "A basic example to easily understand how the framework works.<br/><br/>UML diagram of this example is available <a href=\"http://mvp4g.googlecode.com/svn/trunk/examples/TestMvp4g/TestMvp4g_UML.png\">here</a> (thanks to Steffen Nissen)";
+		menu.addItem( new MenuItem( d1, "testmvp4g", "TestMvp4g", "TestEventBus", false ) );
+
 		//EmployeeAdmin
-		String d2 = "The GWT PureMvc example (as shown <a href=\"http://employeeadm.appspot.com/\">here</a>) but implemented with the Mvp4g framework.<br/>This example also illustrates how to easily test the presenters using directly JUnit (without GWTTestCase).";
-		menu.addItem( new MenuItem( d2, "mvp4gemployeeadmin", "EmployeeAdmin" ) );
-		
+		String d2 = "The GWT PureMvc example (as shown <a href=\"http://employeeadm.appspot.com/\">here</a>) but implemented with the Mvp4g framework.<br/><br/>This example also illustrates how to easily test the presenters using directly JUnit (without GWTTestCase, you can also look at <a href=\"http://groups.google.com/group/mvp4g/browse_thread/thread/82cac05eabe2401b\">this message</a> to see an example with a Mock library).";
+		menu.addItem( new MenuItem( d2, "mvp4gemployeeadmin", "EmployeeAdmin", "EmployeeAdminEventBus", false ) );
+
 		//EmployeeAdmin with GXT
 		String d3 = "Same example as EmployeeAdmin but implemented with GXT using different approaches (<a href=\"http://groups.google.com/group/mvp4g/browse_thread/thread/5ba8664afbbda1f7\">see Mvp4g and GXT post</a>).";
-		menu.addItem( new MenuItem( d3, "mvp4gemployeeadminwithgxt", "EmployeeAdminWithGXT" ) );
-		
+		menu.addItem( new MenuItem( d3, "mvp4gemployeeadminwithgxt", "EmployeeAdminWithGXT", "EmployeeAdminWithGXTEventBus", false ) );
+
 		//Mvp4gHistory
-		String d4 = "This example shows how history is managed thanks to a Place Service. (<a href=\"http://code.google.com/p/mvp4g/wiki/PlaceServiceInMvp4g\">see the wiki page for more information</a>).<br/>It also shows how the Mvp4g configuration file can be simplified (calledMethod and eventObjectClass attributes of event tag are now optional)<br/><br/>" +
-				"<img src=\"beware.gif\" height=15 width=15/> You need to open this application <a href=\"http://mvp4ghistory.appspot.com\">mvp4ghistory.appspot.com</a> in a new window in order to see url modifications.";
-		menu.addItem( new MenuItem( d4, "mvp4ghistory", "Mvp4gHistory" ) );
+		String d4 = "This example shows how history is managed thanks to a Place Service. (<a href=\"http://code.google.com/p/mvp4g/wiki/PlaceService\">see the wiki page for more information</a>).<br/><br/>"
+				+ "<img src=\"beware.gif\" height=15 width=15/> You need to open this application <a href=\"http://mvp4ghistory.appspot.com\">mvp4ghistory.appspot.com</a> in a new window in order to see url modifications.";
+		menu.addItem( new MenuItem( d4, "mvp4ghistory", "Mvp4gHistory", "MyEventBus", false ) );
+
+		String d5 = "This example demonstrates how you can divide your application into modules and take advantage of GWT 2.0 Code splitting feature (<a href=\"http://code.google.com/p/mvp4g/wiki/MultiModules\">see the wiki page for more information</a>)<br/><br/>"
+				+ "It also shows how history is automatically managed with sub-modules (<a href=\"http://code.google.com/p/mvp4g/wiki/PlaceService\">see the wiki page for more information</a>).<br/><br/>"
+				+ "This example has been implemented with an Event Bus interface, you can also find a version implemented with an XML configuration file.<br/><br/>"
+				+ "<img src=\"beware.gif\" height=15 width=15/> You need to open this application <a href=\"http://mvp4gmodules.appspot.com\">mvp4gmodules.appspot.com</a> in a new window in order to see url modifications.";
+		menu.addItem( new MenuItem( d5, "mvp4gmodules", "Mvp4gModules", "main/MainEventBus", true ) );
+
+		String d6 = "This example shows the GWT Mail example converted with Mvp4g. It illustrates how Mvp4g can be used with UiBinder.";
+		menu.addItem( new MenuItem( d6, "http://gwt.google.com/samples/Mail/Mail.html", "MailWithMvp4g", "MailEventBus", false ) );
+
+		String d7 = "This is a really simple example to show how you can use an external library to make non-RPC calls to the server.<br/><br/>"
+		+ "<img src=\"beware.gif\" height=15 width=15/> Because of Google App, the answer to the \"ping\" button may not work right away.";
+		menu.addItem( new MenuItem( d7, "mvp4gedgebox", "Mvp4gWithEdgebox", "MainEventBus", false ) );
+
 	}
 }
