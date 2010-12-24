f920c6cf334d390ebfe6e46f59430b112ae88b9d
plcoirier <plcoirier@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2010-12-24 15:31:02 +0000

    add project to test atp

---
+++ examples/TestAnnotationProcessor/src/com/mvp4g/example/client/history/HistoryConverter1.java
@@ -0,0 +1,24 @@
+package com.mvp4g.example.client.history;
+
+import com.mvp4g.client.annotation.History;
+import com.mvp4g.client.event.EventBus;
+import com.mvp4g.client.history.HistoryConverter;
+
+@History
+public class HistoryConverter1 implements HistoryConverter<EventBus> {
+
+	public String onEventHistoryOk() {
+		return null;
+	}
+
+	@Override
+	public void convertFromToken( String eventType, String param, EventBus eventBus ) {
+
+	}
+
+	@Override
+	public boolean isCrawlable() {
+		return false;
+	}
+
+}
---
+++ examples/TestAnnotationProcessor/src/com/mvp4g/example/client/presenters/Handler5.java
@@ -0,0 +1,10 @@
+package com.mvp4g.example.client.presenters;
+
+import com.mvp4g.client.annotation.EventHandler;
+import com.mvp4g.client.event.BaseEventHandler;
+import com.mvp4g.client.event.EventBusWithLookup;
+
+@EventHandler
+public class Handler5 extends BaseEventHandler<EventBusWithLookup> {
+
+}
