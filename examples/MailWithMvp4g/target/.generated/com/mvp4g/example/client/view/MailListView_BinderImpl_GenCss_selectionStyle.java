e308f36fce956c7a03573a21e8bed66d13030ca7
plcoirier <plcoirier@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2011-03-05 20:35:27 +0000

    updating examples

--- examples/MailWithMvp4g/target/.generated/com/mvp4g/example/client/view/MailListView_BinderImpl_GenCss_selectionStyle.java
+++
@@ -1,6 +0,0 @@
-package com.mvp4g.example.client.view;
-
-import com.mvp4g.example.client.view.MailListView.SelectionStyle;
-
-public interface MailListView_BinderImpl_GenCss_selectionStyle extends SelectionStyle {
-}
