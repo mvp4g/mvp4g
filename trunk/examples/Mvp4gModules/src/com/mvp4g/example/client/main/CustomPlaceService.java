afbd16e47409fbbee1a606d5e204ba594760e1c0
plcoirier <plcoirier@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2012-02-18 13:48:02 +0000

    update Mvp4gModules example for 1.4.0

--- trunk/examples/Mvp4gModules/src/com/mvp4g/example/client/main/CustomPlaceService.java
+++ trunk/examples/Mvp4gModules/src/com/mvp4g/example/client/main/CustomPlaceService.java
@@ -2,10 +2,10 @@ package com.mvp4g.example.client.main;
 
 import com.mvp4g.client.history.PlaceService;
 
-public abstract class CustomPlaceService extends PlaceService {
-	
+public class CustomPlaceService extends PlaceService {
+
 	@Override
-	public String tokenize(String eventName, String param){
+	public String tokenize( String eventName, String param ) {
 		//always add the paramSeparator since "/" is used for module separator and paramSeparator
 		String token = eventName + getParamSeparator();
 		if ( ( param != null ) && ( param.length() > 0 ) ) {
@@ -13,7 +13,7 @@ public abstract class CustomPlaceService extends PlaceService {
 		}
 		return token;
 	}
-	
+
 	protected String getParamSeparator() {
 		return "/";
 	}
