33a65762360d15e84becc1a58db5ab264cedd946
frank.hossfeld@googlemail.com <frank.hossfeld@googlemail.com@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2014-04-23 06:33:50 +0000

    Replacing Generator class with IncrementalGenerator class for use wih SDM

--- sources/src/main/java/com/mvp4g/util/Mvp4gGenerator.java
+++ sources/src/main/java/com/mvp4g/util/Mvp4gGenerator.java
@@ -15,19 +15,11 @@
  */
 package com.mvp4g.util;
 
-import java.io.PrintWriter;
-import java.lang.annotation.Annotation;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.RunAsyncCallback;
-import com.google.gwt.core.ext.Generator;
-import com.google.gwt.core.ext.GeneratorContext;
-import com.google.gwt.core.ext.TreeLogger;
-import com.google.gwt.core.ext.UnableToCompleteException;
+import com.google.gwt.core.ext.*;
 import com.google.gwt.core.ext.typeinfo.JClassType;
+import com.google.gwt.core.ext.typeinfo.JRealClassType;
 import com.google.gwt.core.ext.typeinfo.TypeOracle;
 import com.google.gwt.inject.client.GinModules;
 import com.google.gwt.inject.client.Ginjector;
@@ -39,115 +31,308 @@ import com.mvp4g.client.Mvp4gEventPasser;
 import com.mvp4g.client.Mvp4gException;
 import com.mvp4g.client.Mvp4gModule;
 import com.mvp4g.client.Mvp4gRunAsync;
-import com.mvp4g.client.annotation.EventHandler;
-import com.mvp4g.client.annotation.Events;
-import com.mvp4g.client.annotation.History;
-import com.mvp4g.client.annotation.Presenter;
-import com.mvp4g.client.annotation.Service;
+import com.mvp4g.client.annotation.*;
 import com.mvp4g.client.event.BaseEventBus;
 import com.mvp4g.client.event.EventBus;
 import com.mvp4g.client.event.EventFilter;
 import com.mvp4g.client.event.EventHandlerInterface;
-import com.mvp4g.client.history.HistoryConverter;
-import com.mvp4g.client.history.NavigationConfirmationInterface;
-import com.mvp4g.client.history.NavigationEventCommand;
-import com.mvp4g.client.history.PlaceService;
+import com.mvp4g.client.history.*;
 import com.mvp4g.client.presenter.PresenterInterface;
 import com.mvp4g.util.config.Mvp4gConfiguration;
+import com.mvp4g.util.config.element.EventBusElement;
+import com.mvp4g.util.config.element.Mvp4gElement;
 import com.mvp4g.util.exception.InvalidMvp4gConfigurationException;
+
+import java.io.PrintWriter;
+import java.lang.annotation.Annotation;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Class uses to create the implementation class of Mvp4gStarter
- * 
+ *
  * @author plcoirier
- * 
  */
-public class Mvp4gGenerator extends Generator {
+public class Mvp4gGenerator
+  extends IncrementalGenerator {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.google.gwt.core.ext.Generator#generate(com.google.gwt.core.ext.TreeLogger,
-	 * com.google.gwt.core.ext.GeneratorContext, java.lang.String)
-	 */
-	@Override
-	public String generate( TreeLogger logger, GeneratorContext context, String typeName ) throws UnableToCompleteException {
+  /*
+   * A version id. Increment this as needed, when structural changes are made to
+   * the generated output, specifically with respect to it's effect on the
+   * caching and reuse of previous generator results. Previously cached
+   * generator results will be invalidated automatically if they were generated
+   * by a version of this generator with a different version id.
+   */
+  private static final long GENERATOR_VERSION_ID = 1L;
 
-		Date start = new Date();
 
-		String generatedClassQualifiedName;
+  @Override
+  public RebindResult generateIncrementally(TreeLogger logger,
+                                            GeneratorContext generatorContext,
+                                            String moduleClass)
+    throws UnableToCompleteException {
 
-		try {
-			TypeOracle typeOracle = context.getTypeOracle();
+    TypeOracle typeOracle = generatorContext.getTypeOracle();
+    assert (typeOracle != null);
 
-			JClassType module = typeOracle.findType( typeName );
-			if ( module == null ) {
-				logger.log( TreeLogger.ERROR, "Unable to find metadata for type '" + typeName + "'", null );
-				throw new UnableToCompleteException();
-			}
+    JClassType module = typeOracle.findType(moduleClass);
+    if (module == null) {
+      logger.log(TreeLogger.ERROR,
+                 "Unable to find metadata for module class '" + moduleClass + "'");
+      throw new UnableToCompleteException();
+    }
 
-			@SuppressWarnings( "unchecked" )
-			Map<Class<? extends Annotation>, List<JClassType>> scanResult = AnnotationScanner.scan( logger, typeOracle, new Class[] {
-					Presenter.class, History.class, Events.class, Service.class, EventHandler.class } );
+    if (module.isInterface() == null) {
+      logger.log(TreeLogger.ERROR,
+                 "'" + moduleClass + "' is not a interface");
+      throw new UnableToCompleteException();
+    }
 
-			Mvp4gConfiguration configuration = new Mvp4gConfiguration( logger, context );
-			String suffix = "Impl" + configuration.load( module, scanResult );
-			
-			SourceWriter sourceWriter = getSourceWriter( logger, context, module, suffix );
+    TreeLogger moduleLogger = logger.branch(TreeLogger.DEBUG,
+                                            "Generating mvp4g configuration for module class '" + module.getQualifiedSourceName() + "'",
+                                            null);
 
-			generatedClassQualifiedName = module.getParameterizedQualifiedSourceName() + suffix;
+    return create(moduleLogger,
+                  generatorContext,
+                  moduleClass);
+  }
 
-			if ( sourceWriter != null ) {
-				logger.log( TreeLogger.INFO, "Generating source for " + generatedClassQualifiedName + " ", null );
-				Mvp4gConfigurationFileWriter writer = new Mvp4gConfigurationFileWriter( sourceWriter, configuration );
-				writer.writeConf();
-				sourceWriter.commit( logger );
-			}
 
-		} catch ( InvalidMvp4gConfigurationException e ) {
-			logger.log( TreeLogger.ERROR, e.getMessage(), e );
-			throw new UnableToCompleteException();
-		}
+  @Override
+  public long getVersionId() {
+    return GENERATOR_VERSION_ID;
+  }
 
-		Date end = new Date();
 
-		logger.log( TreeLogger.INFO, "Mvp4g Compilation: " + ( end.getTime() - start.getTime() ) + "ms." );
+  private RebindResult create(TreeLogger logger,
+                              GeneratorContext context,
+                              String moduleName)
+    throws UnableToCompleteException {
 
-		return generatedClassQualifiedName;
-	}
+    Date start = new Date();
 
-	private SourceWriter getSourceWriter( TreeLogger logger, GeneratorContext context, JClassType originalType, String suffix )
-			throws UnableToCompleteException {
+    String generatedClassQualifiedName;
 
-		String packageName = originalType.getPackage().getName();
-		String originalClassName = originalType.getSimpleSourceName();
-		String generatedClassName = originalClassName + suffix;
+    try {
+      TypeOracle typeOracle = context.getTypeOracle();
 
-		logger.log( TreeLogger.INFO, "Generating writer for " + packageName + "." + generatedClassName, null );
+      JClassType module = typeOracle.findType(moduleName);
+      if (module == null) {
+        logger.log(TreeLogger.ERROR,
+                   "Unable to find metadata for type '" + moduleName + "'",
+                   null);
+        throw new UnableToCompleteException();
+      }
 
-		PrintWriter printWriter = context.tryCreate( logger, packageName, generatedClassName );
-		if ( printWriter == null ) {
-			return null;
-		}
+      @SuppressWarnings("unchecked")
+      Map<Class<? extends Annotation>, List<JClassType>> scanResult = AnnotationScanner.scan(logger,
+                                                                                             typeOracle,
+                                                                                             new Class[]{Presenter.class,
+                                                                                               History.class,
+                                                                                               Events.class,
+                                                                                               Service.class,
+                                                                                               EventHandler.class}
+      );
 
-		ClassSourceFileComposerFactory classFactory = new ClassSourceFileComposerFactory( packageName, generatedClassName );
+      Mvp4gConfiguration configuration = new Mvp4gConfiguration(logger,
+                                                                context);
+      String suffix = "Impl" + configuration.load(module,
+                                                  scanResult);
 
-		classFactory.addImplementedInterface( originalType.getName() );
-		String[] classesToImport = getClassesToImport();
-		for ( String classToImport : classesToImport ) {
-			classFactory.addImport( classToImport );
-		}
+      generatedClassQualifiedName = module.getParameterizedQualifiedSourceName() + suffix;
 
-		return classFactory.createSourceWriter( context, printWriter );
-	}
+      // check weather there is a usual version or not.
+      if (checkAlreadyGenerated(logger,
+                                context,
+                                configuration)) {
+        // Log
+        logger.log(TreeLogger.INFO,
+                   "reuse already generated files",
+                   null);
+        // stop generating
+        return new RebindResult(RebindMode.USE_EXISTING,
+                                generatedClassQualifiedName);
+      } else {
+        // Log
+        logger.log(TreeLogger.INFO,
+                   "Generate files ... ",
+                   null);
+      }
 
-	String[] getClassesToImport() {
-		return new String[] { PlaceService.class.getName(), GWT.class.getName(), com.google.gwt.user.client.History.class.getName(),
-				ServiceDefTarget.class.getName(), PresenterInterface.class.getName(), EventBus.class.getName(), Mvp4gException.class.getName(),
-				HistoryConverter.class.getName(), Mvp4gEventPasser.class.getName(), Mvp4gModule.class.getName(), GinModules.class.getName(),
-				Ginjector.class.getName(), BaseEventBus.class.getName(), EventFilter.class.getName(), EventHandlerInterface.class.getName(),
-				List.class.getName(), NavigationEventCommand.class.getName(), NavigationConfirmationInterface.class.getName(),
-				RunAsyncCallback.class.getName(), Mvp4gRunAsync.class.getName(), Command.class.getName() };
-	}
+      // No, there is non. Create a new one.
+      SourceWriter sourceWriter = getSourceWriter(logger,
+                                                  context,
+                                                  module,
+                                                  suffix);
 
-}
+      if (sourceWriter != null) {
+        logger.log(TreeLogger.INFO,
+                   "Generating source for " + generatedClassQualifiedName + " ",
+                   null);
+        Mvp4gConfigurationFileWriter writer = new Mvp4gConfigurationFileWriter(sourceWriter,
+                                                                               configuration);
+        writer.writeConf();
+        sourceWriter.commit(logger);
+      } else {
+        // don't expect this to occur, but could happen if an instance was
+        // recently generated but not yet committed
+        new RebindResult(RebindMode.USE_EXISTING,
+                         generatedClassQualifiedName);
+      }
+    } catch (InvalidMvp4gConfigurationException e) {
+      logger.log(TreeLogger.ERROR,
+                 e.getMessage(),
+                 e);
+      throw new UnableToCompleteException();
+    }
+
+    Date end = new Date();
+
+    logger.log(TreeLogger.INFO,
+               "Mvp4g Compilation: " + (end.getTime() - start.getTime()) + "ms.");
+
+    return new RebindResult(RebindMode.USE_ALL_NEW_WITH_NO_CACHING,
+                            generatedClassQualifiedName);
+  }
+
+  private SourceWriter getSourceWriter(TreeLogger logger,
+                                       GeneratorContext context,
+                                       JClassType originalType,
+                                       String suffix)
+    throws UnableToCompleteException {
+
+    String packageName = originalType.getPackage().getName();
+    String originalClassName = originalType.getSimpleSourceName();
+    String generatedClassName = originalClassName + suffix;
+
+    logger.log(TreeLogger.INFO,
+               "Generating writer for " + packageName + "." + generatedClassName,
+               null);
+
+    PrintWriter printWriter = context.tryCreate(logger,
+                                                packageName,
+                                                generatedClassName);
+    if (printWriter == null) {
+      return null;
+    }
+
+    ClassSourceFileComposerFactory classFactory = new ClassSourceFileComposerFactory(packageName,
+                                                                                     generatedClassName);
+
+    classFactory.addImplementedInterface(originalType.getName());
+    String[] classesToImport = getClassesToImport();
+    for (String classToImport : classesToImport) {
+      classFactory.addImport(classToImport);
+    }
+
+    return classFactory.createSourceWriter(context,
+                                           printWriter);
+  }
+
+  String[] getClassesToImport() {
+    return new String[]{com.mvp4g.client.history.PlaceService.class.getName(), GWT.class.getName(), com.google.gwt.user.client.History.class.getName(),
+      ServiceDefTarget.class.getName(), PresenterInterface.class.getName(), EventBus.class.getName(), Mvp4gException.class.getName(),
+      HistoryConverter.class.getName(), Mvp4gEventPasser.class.getName(), Mvp4gModule.class.getName(), GinModules.class.getName(),
+      Ginjector.class.getName(), BaseEventBus.class.getName(), EventFilter.class.getName(), EventHandlerInterface.class.getName(),
+      List.class.getName(), NavigationEventCommand.class.getName(), NavigationConfirmationInterface.class.getName(),
+      RunAsyncCallback.class.getName(), Mvp4gRunAsync.class.getName(), Command.class.getName()};
+  }
+
+  private boolean checkAlreadyGenerated(TreeLogger logger,
+                                        GeneratorContext ctx,
+                                        Mvp4gConfiguration configuration) {
+
+    CachedGeneratorResult lastRebindResult = ctx.getCachedGeneratorResult();
+
+    if (lastRebindResult == null || !ctx.isGeneratorResultCachingEnabled()) {
+      return false;
+    }
+
+    // Check whether all files are up to date
+    // EventBus
+    if (!this.checkEventBus(logger,
+                            ctx)) {
+      return false;
+    }
+    // Module
+    if (!this.checkModule(logger,
+                          ctx,
+                          configuration.getModule())) {
+      return false;
+    }
+    // ChildModuleElement
+    if (!this.checkSet(logger,
+                       ctx,
+                       configuration.getChildModules())) {
+      return false;
+    }
+    // everything ok!  -> use the old one ...
+    return true;
+  }
+
+  private boolean checkSet(TreeLogger logger,
+                           GeneratorContext ctx,
+                           Set<? extends Mvp4gElement> setOfElements) {
+
+    long lastTimeGenerated = ctx.getCachedGeneratorResult().getTimeGenerated();
+
+    for (Mvp4gElement element : setOfElements) {
+      JClassType sourceType = ctx.getTypeOracle().findType(element.getProperty("class"));
+      if (sourceType == null) {
+        logger.log(TreeLogger.TRACE,
+                   "Found previously dependent type that's no longer present: " + element.getProperty("class"));
+        return false;
+      }
+      assert sourceType instanceof JRealClassType;
+      JRealClassType realClass = (JRealClassType) sourceType;
+      if (realClass == null ||
+        realClass.getLastModifiedTime() > lastTimeGenerated) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean checkEventBus(TreeLogger logger,
+                                GeneratorContext ctx) {
+
+    long lastTimeGenerated = ctx.getCachedGeneratorResult().getTimeGenerated();
+
+    JClassType sourceType = ctx.getTypeOracle().findType(EventBusElement.class.getName());
+    if (sourceType == null) {
+      logger.log(TreeLogger.TRACE,
+                 "Found previously dependent type that's no longer present: " + EventBusElement.class.getName());
+      return false;
+    }
+    assert sourceType instanceof JRealClassType;
+    JRealClassType realClass = (JRealClassType) sourceType;
+    if (realClass == null ||
+      realClass.getLastModifiedTime() > lastTimeGenerated) {
+      return false;
+    }
+
+    return true;
+  }
+
+  private boolean checkModule(TreeLogger logger,
+                              GeneratorContext ctx,
+                              JClassType module) {
+
+    long lastTimeGenerated = ctx.getCachedGeneratorResult().getTimeGenerated();
+
+    if (module == null) {
+      logger.log(TreeLogger.TRACE,
+                 "Found previously dependent type that's no longer present: " + Mvp4gModule.class.getName());
+      return false;
+    }
+    assert module instanceof JRealClassType;
+    JRealClassType realClass = (JRealClassType) module;
+    if (realClass == null ||
+      realClass.getLastModifiedTime() > lastTimeGenerated) {
+      return false;
+    }
+
+    return true;
+  }}
--- sources/src/main/java/com/mvp4g/util/Mvp4gRunAsyncGenerator.java
+++ sources/src/main/java/com/mvp4g/util/Mvp4gRunAsyncGenerator.java
@@ -15,92 +15,171 @@
  */
 package com.mvp4g.util;
 
-import java.io.PrintWriter;
-
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.RunAsyncCallback;
-import com.google.gwt.core.ext.Generator;
-import com.google.gwt.core.ext.GeneratorContext;
-import com.google.gwt.core.ext.TreeLogger;
-import com.google.gwt.core.ext.UnableToCompleteException;
+import com.google.gwt.core.ext.*;
 import com.google.gwt.core.ext.typeinfo.JClassType;
 import com.google.gwt.core.ext.typeinfo.JMethod;
 import com.google.gwt.core.ext.typeinfo.TypeOracle;
 import com.google.gwt.user.rebind.ClassSourceFileComposerFactory;
 import com.google.gwt.user.rebind.SourceWriter;
 
+import java.io.PrintWriter;
+import java.util.Date;
+
 /**
  * Class uses to create the implementation class of Mvp4gStarter
- * 
+ *
  * @author plcoirier
- * 
  */
-public class Mvp4gRunAsyncGenerator extends Generator {
+public class Mvp4gRunAsyncGenerator
+  extends IncrementalGenerator {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.google.gwt.core.ext.Generator#generate(com.google.gwt.core.ext.TreeLogger,
-	 * com.google.gwt.core.ext.GeneratorContext, java.lang.String)
-	 */
-	@Override
-	public String generate( TreeLogger logger, GeneratorContext context, String typeName ) throws UnableToCompleteException {
-		TypeOracle typeOracle = context.getTypeOracle();
+  /*
+   * A version id. Increment this as needed, when structural changes are made to
+   * the generated output, specifically with respect to it's effect on the
+   * caching and reuse of previous generator results. Previously cached
+   * generator results will be invalidated automatically if they were generated
+   * by a version of this generator with a different version id.
+   */
+  private static final long GENERATOR_VERSION_ID = 1L;
 
-		JClassType originalType = typeOracle.findType( typeName );
-		if ( originalType == null ) {
-			logger.log( TreeLogger.ERROR, "Unable to find metadata for type '" + typeName + "'", null );
-			throw new UnableToCompleteException();
-		}
+  @Override
+  public RebindResult generateIncrementally(TreeLogger logger,
+                                            GeneratorContext generatorContext,
+                                            String typeName)
+    throws UnableToCompleteException {
 
-		String packageName = originalType.getPackage().getName();
-		String originalClassName = originalType.getSimpleSourceName();
-		String generatedClassName = originalClassName + "Impl";
+    TypeOracle typeOracle = generatorContext.getTypeOracle();
+    assert (typeOracle != null);
 
-		logger.log( TreeLogger.INFO, "Generating writer for " + packageName + "." + generatedClassName, null );
+    JClassType originalType = typeOracle.findType(typeName);
+    if (originalType == null) {
+      logger.log(TreeLogger.ERROR,
+                 "Unable to find metadata for type '" + typeName + "'");
+      throw new UnableToCompleteException();
+    }
 
-		PrintWriter printWriter = context.tryCreate( logger, packageName, generatedClassName );
+    if (originalType.isInterface() == null) {
+      logger.log(TreeLogger.ERROR,
+                 "'" + typeName + "' is not a interface");
+      throw new UnableToCompleteException();
+    }
 
-		ClassSourceFileComposerFactory classFactory = new ClassSourceFileComposerFactory( packageName, generatedClassName );
+    TreeLogger moduleLogger = logger.branch(TreeLogger.DEBUG,
+                                            "Generating client proxy for remote service interface '" + originalType.getQualifiedSourceName() + "'",
+                                            null);
 
-		classFactory.addImplementedInterface( originalType.getName() );
-		String[] classesToImport = getClassesToImport();
-		for ( String classToImport : classesToImport ) {
-			classFactory.addImport( classToImport );
-		}
+    return create(originalType,
+                  moduleLogger,
+                  generatorContext,
+                  typeName);
+  }
 
-		String generatedClassQualifiedName = packageName + "." + generatedClassName;
-		if ( printWriter != null ) {
-			SourceWriter sourceWriter = classFactory.createSourceWriter( context, printWriter );
-			logger.log( TreeLogger.INFO, "Generating source for " + generatedClassQualifiedName + " ", null );
-			writeClass( sourceWriter, getRunAsync( originalType ) );
-			sourceWriter.commit( logger );
-		}
+  @Override
+  public long getVersionId() {
+    return GENERATOR_VERSION_ID;
+  }
 
-		return generatedClassQualifiedName;
-	}
 
-	void writeClass( SourceWriter sourceWriter, String callBackType) {
-		sourceWriter.print( "public void load(" );
-		sourceWriter.print( callBackType );
-		sourceWriter.println( " callback) {" );
-		sourceWriter.indent();
-		sourceWriter.println( "GWT.runAsync(callback);" );
-		sourceWriter.outdent();
-		sourceWriter.println( "}" );
-	}
+  private RebindResult create(JClassType originalType,
+                              TreeLogger logger,
+                              GeneratorContext context,
+                              String typeName)
+    throws UnableToCompleteException {
 
-	String[] getClassesToImport() {
-		return new String[] { GWT.class.getName(), RunAsyncCallback.class.getName() };
-	}
+    Date start = new Date();
 
-	String getRunAsync( JClassType originalType ) {
-		JMethod[] methods = originalType.getOverridableMethods();
-		for ( JMethod method : methods ) {
-			if ( "load".equals( method.getName() ) ) {
-				return method.getParameters()[0].getType().getQualifiedSourceName();
-			}
-		}
-		return null;
-	}
+    String packageName = originalType.getPackage().getName();
+    String originalClassName = originalType.getSimpleSourceName();
+    String generatedClassName = originalClassName + "Impl";
+    String generatedClassQualifiedName = packageName + "." + generatedClassName;
+
+    // check weather there is a usual version or not.
+    if (checkAlreadyGenerated(logger,
+                              context,
+                              generatedClassQualifiedName)) {
+      // Log
+      logger.log(TreeLogger.INFO,
+                 "reuse already generated files",
+                 null);
+      // stop generating
+      return new RebindResult(RebindMode.USE_EXISTING,
+                              generatedClassQualifiedName);
+    }
+
+    logger.log(TreeLogger.INFO,
+               "Generating writer for " + packageName + "." + generatedClassName,
+               null);
+
+    PrintWriter printWriter = context.tryCreate(logger,
+                                                packageName,
+                                                generatedClassName);
+
+    ClassSourceFileComposerFactory classFactory = new ClassSourceFileComposerFactory(packageName,
+                                                                                     generatedClassName);
+
+    classFactory.addImplementedInterface(originalType.getName());
+    String[] classesToImport = getClassesToImport();
+    for (String classToImport : classesToImport) {
+      classFactory.addImport(classToImport);
+    }
+
+    if (printWriter != null) {
+      SourceWriter sourceWriter = classFactory.createSourceWriter(context,
+                                                                  printWriter);
+      logger.log(TreeLogger.INFO,
+                 "Generating source for " + generatedClassQualifiedName + " ",
+                 null);
+      writeClass(sourceWriter,
+                 getRunAsync(originalType));
+      sourceWriter.commit(logger);
+    }
+
+    Date end = new Date();
+
+    logger.log(TreeLogger.INFO,
+               "Mvp4g Module Cretor: " + (end.getTime() - start.getTime()) + "ms.");
+
+    return new RebindResult(RebindMode.USE_ALL_NEW_WITH_NO_CACHING,
+                            generatedClassQualifiedName);
+  }
+
+  private boolean checkAlreadyGenerated(TreeLogger logger,
+                                        GeneratorContext ctx,
+                                        String generatedClassQualifiedName) {
+
+    CachedGeneratorResult lastRebindResult = ctx.getCachedGeneratorResult();
+
+    if (lastRebindResult == null || !ctx.isGeneratorResultCachingEnabled()) {
+      return false;
+    }
+    /* it looks like we could use the existing one */
+    return true;
+  }
+
+  void writeClass(SourceWriter sourceWriter,
+                  String callBackType) {
+    sourceWriter.print("public void load(");
+    sourceWriter.print(callBackType);
+    sourceWriter.println(" callback) {");
+    sourceWriter.indent();
+    sourceWriter.println("GWT.runAsync(callback);");
+    sourceWriter.outdent();
+    sourceWriter.println("}");
+  }
+
+  String[] getClassesToImport() {
+    return new String[]{GWT.class.getName(), RunAsyncCallback.class.getName()};
+  }
+
+  String getRunAsync(JClassType originalType) {
+    JMethod[] methods = originalType.getOverridableMethods();
+    for (JMethod method : methods) {
+      if ("load".equals(method.getName())) {
+        return method.getParameters()[0].getType().getQualifiedSourceName();
+      }
+    }
+    return null;
+  }
 }
