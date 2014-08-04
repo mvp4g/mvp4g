4748d11493c10ccc922798d11c42f5a60ac74e31
frank.hossfeld@googlemail.com <frank.hossfeld@googlemail.com@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2014-08-04 11:03:20 +0000

    EmployeeAdmin: removed NPE in case of new user

--- trunk/examples/EmployeeAdmin/src/com/mvp4g/example/client/ui/user/profile/UserProfileView.java
+++ trunk/examples/EmployeeAdmin/src/com/mvp4g/example/client/ui/user/profile/UserProfileView.java
@@ -194,6 +194,8 @@ public class UserProfileView
     username.setValue("");
     password.setValue("");
     confirmPassword.setValue("");
+
+    department.setValue(null);
   }
 
   private void enableUpdateButton() {
