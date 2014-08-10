6a325c76b0e773709172ae794a32b2d800eee06b
frank.hossfeld@googlemail.com <frank.hossfeld@googlemail.com@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2014-08-10 16:03:31 +0000

    AppEnginge-Support

--- trunk/examples/Mvp4gShowCase/src/com/mvp4g/example/client/MenuItem.java
+++ trunk/examples/Mvp4gShowCase/src/com/mvp4g/example/client/MenuItem.java
@@ -2,41 +2,46 @@ package com.mvp4g.example.client;
 
 import com.google.gwt.user.client.ui.TreeItem;
 
-public class MenuItem extends TreeItem {
+public class MenuItem
+    extends TreeItem {
 
-	private String description = null;
-	private String appId = null;
-	private String projectName = null;
-	private String eventBusName = null;
-	private boolean withXml = false;
+  private String  description  = null;
+  private String  appId        = null;
+  private String  projectName  = null;
+  private String  eventBusName = null;
+  private boolean withXml      = false;
 
-	public MenuItem( String description, String appId, String projectName, String eventBusName, boolean withXml ) {
-        this.setText(projectName);
-		this.description = description;
-		this.appId = appId;
-		this.projectName = projectName;
-		this.eventBusName = eventBusName;
-		this.withXml = withXml;
-	}
+  public MenuItem(String description,
+                  String appId,
+                  String projectName,
+                  String eventBusName,
+                  boolean withXml) {
+    this.setText(projectName);
+    this.description = description;
+    this.appId = appId;
+    this.projectName = projectName;
+    this.eventBusName = eventBusName;
+    this.withXml = withXml;
+  }
 
-	public String getDescription() {
-		return description;
-	}
+  public String getDescription() {
+    return description;
+  }
 
-	public String getAppId() {
-		return appId;
-	}
+  public String getAppId() {
+    return appId;
+  }
 
-	public String getProjectName() {
-		return projectName;
-	}
+  public String getProjectName() {
+    return projectName;
+  }
 
-	public String getEventBusName() {
-		return eventBusName;
-	}
+  public String getEventBusName() {
+    return eventBusName;
+  }
 
-	public boolean isWithXml() {
-		return withXml;
-	}
+  public boolean isWithXml() {
+    return withXml;
+  }
 
 }
--- trunk/examples/Mvp4gShowCase/src/com/mvp4g/example/client/MiddlePanel.java
+++ trunk/examples/Mvp4gShowCase/src/com/mvp4g/example/client/MiddlePanel.java
@@ -1,57 +1,61 @@
 package com.mvp4g.example.client;
 
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Frame;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.TabPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.*;
 
-public class MiddlePanel extends Composite {
+public class MiddlePanel
+    extends Composite {
 
-	private HTML description = new HTML();
-	private Frame demo = new Frame();
-	private Frame eventbus = new Frame();
-	private Frame configuration = new Frame();
-	private TabPanel tabPanel = new TabPanel();
+  private HTML     description   = new HTML();
+  private Frame    demo          = new Frame();
+//  private Frame    eventbus      = new Frame();
+//  private Frame    configuration = new Frame();
+//  private TabPanel tabPanel      = new TabPanel();
 
-	public MiddlePanel() {
+  public MiddlePanel() {
 
-		VerticalPanel mainPanel = new VerticalPanel();
-		initWidget( mainPanel );
+    VerticalPanel mainPanel = new VerticalPanel();
+    initWidget(mainPanel);
 
-		mainPanel.add( description );
+    mainPanel.add(description);
 
-		tabPanel.add( demo, "Demo" );
-		tabPanel.add( eventbus, "Event Bus Interface" );
+    demo.setStyleName("demo-frame");
 
-		mainPanel.add( tabPanel );
+//    tabPanel.add(demo,
+//                 "Demo");
+//    tabPanel.add(eventbus,
+//                 "Event Bus Interface");
+//
+//    mainPanel.add(tabPanel);
+    mainPanel.add(demo);
 
-	}
+  }
 
-	public void set( MenuItem item ) {
-		description.setHTML( "<h2>" + item.getProjectName() + "</h2>" + item.getDescription() );
-		String appId = item.getAppId();
-		if ( appId.startsWith( "http" ) ) {
-			demo.setUrl( appId );
-		} else {
-			demo.setUrl( "http://" + item.getAppId() + ".appspot.com/" );
-		}
-		eventbus.setUrl( "http://code.google.com/p/mvp4g/source/browse/tags/mvp4g-1.1.0/examples/" + item.getProjectName()
-				+ "/src/com/mvp4g/example/client/" + item.getEventBusName() + ".java" );
-		tabPanel.selectTab( 0 );
+  public void set(MenuItem item) {
+    description.setHTML("<h2>" + item.getProjectName() + "</h2>" + item.getDescription());
+    String appId = item.getAppId();
+    if (appId.startsWith("http")) {
+      demo.setUrl(appId);
+    } else {
+      demo.setUrl("http://" + item.getAppId() + ".appspot.com/");
+    }
+//    eventbus.setUrl("http://code.google.com/p/mvp4g/source/browse/tags/mvp4g-1.5.0/examples/" + item.getProjectName()
+//                    + "/src/com/mvp4g/example/client/" + item.getEventBusName() + ".java");
+//    tabPanel.selectTab(0);
+//
+//    int nbTab = tabPanel.getTabBar()
+//                        .getTabCount();
+//    if (!item.isWithXml()) {
+//      if (nbTab == 3) {
+//        tabPanel.remove(configuration);
+//      }
+//    } else {
+//      configuration.setUrl("http://mvp4g.googlecode.com/svn/tags/mvp4g-1.5.0/examples/" + item.getProjectName() + "Xml/src/mvp4g-conf.xml");
+//      if (nbTab < 3) {
+//        tabPanel.add(configuration,
+//                     "Mvp4g XML Configuration");
+//      }
+//    }
 
-		int nbTab = tabPanel.getTabBar().getTabCount();
-		if ( !item.isWithXml() ) {
-			if ( nbTab == 3 ) {
-				tabPanel.remove( configuration );
-			}
-		} else {
-			configuration.setUrl( "http://mvp4g.googlecode.com/svn/tags/mvp4g-1.1.0/examples/" + item.getProjectName() + "Xml/src/mvp4g-conf.xml" );
-			if ( nbTab < 3 ) {
-				tabPanel.add( configuration, "Mvp4g XML Configuration" );
-			}
-		}
-
-	}
+  }
 
 }
