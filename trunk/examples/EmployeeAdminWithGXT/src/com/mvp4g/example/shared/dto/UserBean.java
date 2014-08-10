9efa3b6fbe3b59b857aa33e44b2cab8898fd1e3a
frank.hossfeld@googlemail.com <frank.hossfeld@googlemail.com@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2014-08-10 12:40:21 +0000

    - GXT example implemented

--- trunk/examples/EmployeeAdminWithGXT/src/com/mvp4g/example/shared/dto/UserBean.java
+++ trunk/examples/EmployeeAdminWithGXT/src/com/mvp4g/example/shared/dto/UserBean.java
@@ -99,30 +99,6 @@ public class UserBean
 
   @Override
   public String toString() {
-    return new StringBuilder().append("UserBean{")
-                              .append("id=")
-                              .append(id)
-                              .append(", firstName='")
-                              .append(firstName == null ? "" : firstName)
-                              .append('\'')
-                              .append(", lastName='")
-                              .append(lastName == null ? "" : lastName)
-                              .append('\'')
-                              .append(", email='")
-                              .append(email == null ? "" : email)
-                              .append('\'')
-                              .append(", username='")
-                              .append(username == null ? "" : username)
-                              .append('\'')
-                              .append(", password='")
-                              .append(password == null ? "" : password)
-                              .append('\'')
-                              .append(", department='")
-                              .append(department == null ? "" : department)
-                              .append('\'')
-                              .append(", roles=")
-                              .append(roles == null ? "" : roles.toString())
-                              .append('}')
-                              .toString();
+    return "UserBean{" + "id=" + id + ", firstName='" + (firstName == null ? "" : firstName) + '\'' + ", lastName='" + (lastName == null ? "" : lastName) + '\'' + ", email='" + (email == null ? "" : email) + '\'' + ", username='" + (username == null ? "" : username) + '\'' + ", password='" + (password == null ? "" : password) + '\'' + ", department='" + (department == null ? "" : department) + '\'' + ", roles=" + (roles == null ? "" : roles.toString()) + '}';
   }
 }
