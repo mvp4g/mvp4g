985f07f5dd5c5bb668bd56537769dc2f0721855c
frank.hossfeld@googlemail.com <frank.hossfeld@googlemail.com@d76a6fa2-5909-11de-b36e-3fc19fc75de0>
2015-01-19 07:39:44 +0000

    start issue 122

---
+++ trunk/sources/src/main/java/org/gwt4e/mvp4g/application/client/helper/Mvp4gShell.java
@@ -0,0 +1,25 @@
+package org.gwt4e.mvp4g.application.client.helper;
+
+import com.mvp4g.client.event.EventBus;
+
+// TODO documenation
+public interface Mvp4gShell<E extends EventBus, V> {
+
+	/**
+	 * Called before a module/code splitter is loaded. To start the code loading, the load command
+	 * must be executed. This way, asynchronous action can be made.
+	 * 
+	 * @param eventBus
+	 *            The event bus loading the code (ie in case of child module, the parent event bus).
+	 * @param eventName
+	 *            The name of the event that requested the module to be loaded, null if the request
+	 *            come from a history token change.
+	 * @param params
+	 *            The objects fired with this event, null is no object are fired with the event or
+	 *            if the request come from a history token change.
+	 * @param load
+	 *            The command to execute when the module needs to be loaded.
+	 */
+	void load(E eventBus);
+
+}
