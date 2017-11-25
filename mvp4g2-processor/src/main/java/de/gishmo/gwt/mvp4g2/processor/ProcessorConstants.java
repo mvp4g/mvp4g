08d59e8ef3130c44f8fdb10ce8a9465765e64eea
Frank Hossfeld <frank.hossfeld@googlemail.com>
2017-11-25 21:35:50 +0100

    remove files

--- mvp4g2-processor/src/main/java/de/gishmo/gwt/mvp4g2/processor/ProcessorConstants.java
+++
@@ -1,9 +0,0 @@
-package de.gishmo.gwt.mvp4g2.processor;
-
-public class ProcessorConstants {
-
-  public final static String IMPL_NAME       = "Impl";
-  public final static String EVENT_META_DATA = "EventMetaData";
-  public final static String META_DATA       = "MetaData";
-
-}
--- mvp4g2/src/main/java/de/gishmo/gwt/mvp4g2/client/eventbus/PresenterRegistration.java
+++
@@ -1,7 +0,0 @@
-package de.gishmo.gwt.mvp4g2.client.eventbus;
-
-public interface PresenterRegistration {
-
-  void remove();
-
-}
